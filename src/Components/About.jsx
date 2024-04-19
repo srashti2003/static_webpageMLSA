2f23690f5542648367d16e161eb06f55e70e51a0
Srashti Gupta <109365275+srashti2003@users.noreply.github.com>
2024-04-19 16:26:37 +0000

    home and about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/second.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "a smooth sand background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Pre-Final Engineering Student specialized in Artificial Intelligence and Data Science and a dedicated Competitive Programmer.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "Machine Learning",
+  "Data Analysis",
+  "10-keys Typing",
+  "Front-End Web Development",
+  "Canva",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my experience in developement also I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio-page.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "organized desk with books and laptop";
 
 /**
  * Project list

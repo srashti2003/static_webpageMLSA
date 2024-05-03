02dab0518ec16946afd2deccf685e0642bfe70e1
Shreyank Agrawal <shreyankagrawal17@gmail.com>
2024-05-03 23:04:14 +0530

    The final chane

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-import Education from "./components/Education";
+import Education from "./Components/Education";
 import Portfolio from "./Components/Portfolio";
 
 import "./styles.css";
